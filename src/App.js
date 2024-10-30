b781950cb4a044c18e9936630f12ae794bb4977a
lajkogege <lajkogege@gmail.com>
2024-10-30 14:37:45 +0100

    alap

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
+
+// Importáljuk az App.css fájlt, amely az App komponens stílusait tartalmazza
 import './App.css';
 
+import Book from './Book';
+
+// Definiáljuk az App komponenst, amely a fő komponens lesz a React alkalmazásban
 function App() {
   return (
+    // A gyökér div, amely az "App" osztályt használja a stílusokhoz
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Book title="Avatár" />
+      <Book title="Kis herceg" />
     </div>
   );
 }
 
+// Az App komponens exportálása, hogy más fájlokban is használható legyen
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
+// Importáljuk a React könyvtárat, amely lehetővé teszi a React funkciók és a JSX használatát
 import React from 'react';
+
+// Importáljuk a ReactDOM könyvtárat, amely felelős a React elemek megjelenítéséért a böngészőben
 import ReactDOM from 'react-dom/client';
+
+// Importáljuk az index.css fájlt, amely az alkalmazás globális stílusait tartalmazza
 import './index.css';
+
+// Importáljuk az App komponenst, ami a fő alkalmazáskomponens
 import App from './App';
+
+// Importáljuk a reportWebVitals modult, amely lehetőséget ad az alkalmazás teljesítményének mérésére
 import reportWebVitals from './reportWebVitals';
 
+// Létrehozunk egy "root" (gyökér) elemet, ami az index.html-ben található 'root' ID-jú elemet célozza meg
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+
+// Megjelenítjük az App komponenst a root elemben, és StrictMode-ba helyezzük
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// Az alkalmazás teljesítményének méréséhez a reportWebVitals funkciót használhatjuk
+// Például átadhatunk egy logoló függvényt, hogy eredményeket kapjunk (pl.: reportWebVitals(console.log))
+// További részletek: https://bit.ly/CRA-vitals
 reportWebVitals();
